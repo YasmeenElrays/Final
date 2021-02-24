6e74359a518cc7eb22cd5ab41492b42923938d23
YasmeenElrays <70237097+YasmeenElrays@users.noreply.github.com>
2021-02-24 10:05:05 +0200

    upload files

---
+++ LoginTCs.java
@@ -0,0 +1,15 @@
+package Tests;
+
+import DriverBase.Base;
+import Pages.UserLogin;
+import org.testng.annotations.Test;
+
+public class LoginTCs extends Base {
+    @Test(alwaysRun = true)
+    public void Login(){
+        UserLogin Login=new UserLogin(driver);
+        Login.LoginPage();
+       // Login.newPopupCloseBtn.click();
+    }
+
+}
